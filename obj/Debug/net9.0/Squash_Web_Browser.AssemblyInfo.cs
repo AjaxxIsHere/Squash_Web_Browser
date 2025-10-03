a7d6bf393e1ae2e831b4d8ada80c2ebcbd3b7b81
ajaz <ajaz2468@gmail.com>
2025-10-03 17:13:32 +0400

    Refactor application structure: remove old App and MainWindow files, add new implementations in Views directory

--- obj/Debug/net9.0/Squash_Web_Browser.AssemblyInfo.cs
+++ obj/Debug/net9.0/Squash_Web_Browser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Squash_Web_Browser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0babf2b4330bdfdb2a41f7f831e9909475b9aa05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c15095860494f77cc269ab61e8e6f6b8aaf4db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Squash_Web_Browser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Squash_Web_Browser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
